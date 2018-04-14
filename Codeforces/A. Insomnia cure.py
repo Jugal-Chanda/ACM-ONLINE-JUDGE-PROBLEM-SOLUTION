ab69a5e34529cf00762bc07186fea32bb0eea0a4
Jugal Kishore Chanda <34421749+jugal1234@users.noreply.github.com>
2018-04-14 21:36:58 +0600

    Create A. Insomnia cure.py

---
+++ Codeforces/A. Insomnia cure.py
@@ -0,0 +1,19 @@
+#Insomnia_cure.py
+
+k,l,m,n = int(input()),int(input()),int(input()),int(input())
+d= int(input())
+
+mark_list = [0]*(d+1)
+
+for i in range(k,d+1,k):
+    mark_list[i] = 1
+for i in range(l,d+1,l):
+    mark_list[i] = 1
+for i in range(m,d+1,m):
+    mark_list[i] = 1
+for i in range(n,d+1,n):
+    mark_list[i] = 1
+ans = mark_list.count(1)
+print(ans)
+
+        
