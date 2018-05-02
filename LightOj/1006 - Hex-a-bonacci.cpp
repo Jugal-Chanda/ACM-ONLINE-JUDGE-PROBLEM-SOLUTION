108fdcbab1de7b05fd24cef07ff689c2c94cf463
Jugal Kishore Chanda <34421749+jugal1234@users.noreply.github.com>
2018-05-03 00:00:38 +0600

    Create 1006 - Hex-a-bonacci.cpp

---
+++ LightOj/1006 - Hex-a-bonacci.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long int ara[10010];
+int a,b,c,d,e,f,n;
+void fn()
+{
+    ara[0] = a;
+    ara[1] = b;
+    ara[2] = c;
+    ara[3] = d;
+    ara[4] = e;
+    ara[5] = f;
+    for(int i=6;i<=n;i++)
+    {
+        long long int sum = 0;
+        for(int j=i-6;j<i;j++)
+        {
+            sum+=ara[j]%10000007;
+        }
+        ara[i] = sum%10000007;
+    }
+}
+int main()
+{
+    int caseno = 0, cases;
+    scanf("%d", &cases);
+    while( cases-- ) {
+        scanf("%d %d %d %d %d %d %d", &a, &b, &c, &d, &e, &f, &n);
+        fn();
+        printf("Case %d: %lld\n", ++caseno, ara[n] % 10000007);
+    }
+    return 0;
+}
+ 
